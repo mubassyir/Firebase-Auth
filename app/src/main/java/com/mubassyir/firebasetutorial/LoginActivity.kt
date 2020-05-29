e1e4d758b133951c034e58277fc274b1718956e3
Mubassyir <amubassyir@gmail.com>
2020-05-29 08:34:14 +0700

    Creating New Project and designing login and register layout

--- app/src/main/java/com/mubassyir/firebasetutorial/LoginActivity.kt
+++ app/src/main/java/com/mubassyir/firebasetutorial/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.mubassyir.firebasetutorial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        text_view_register.setOnClickListener{
+            Intent(this,RegisterActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/mubassyir/firebasetutorial/RegisterActivity.kt
+++ app/src/main/java/com/mubassyir/firebasetutorial/RegisterActivity.kt
@@ -1,12 +1,20 @@
 package com.mubassyir.firebasetutorial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        text_view_login.setOnClickListener{
+            Intent(this,LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
