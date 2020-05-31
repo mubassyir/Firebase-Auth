3bd7febdf6cfa84163a59ae13360481452113d82
Mubassyir <amubassyir@gmail.com>
2020-05-31 14:03:55 +0700

    Adding reset password activity

--- app/src/main/java/com/mubassyir/firebasetutorial/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/mubassyir/firebasetutorial/ui/fragments/HomeFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.mubassyir.firebasetutorial.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,5 +24,17 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        displayName.text = currentUser?.email
+
+        Glide.with(this)
+            .load(currentUser?.photoUrl)
+            .into(profilePicture)
+
+    }
 
 }
